e5e13526d0d157da985fa7ef6aaa9a80d8a55b15
Keifer Erikson <keifer@erikson.dev>
2024-07-27 12:43:24 -0600

    Add hardtime

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Add this to your Neovim configuration
+-- vim.api.nvim_set_keymap("i", ":w", "<Esc>:w<CR>", { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap("i", ":wq", "<Esc>:wq<CR>", { noremap = true, silent = true })
